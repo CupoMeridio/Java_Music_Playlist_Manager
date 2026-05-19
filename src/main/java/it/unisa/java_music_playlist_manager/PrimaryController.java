0a13d77c70120ddaa80f946f8c3032077454ef8d
CupoMeridio <cupo-meridio@live.it>
2026-05-19 14:52:31 +0200

    Configurazione iniziale del progetto Netbeans

---
+++ src/main/java/it/unisa/java_music_playlist_manager/PrimaryController.java
@@ -0,0 +1,12 @@
+package it.unisa.java_music_playlist_manager;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class PrimaryController {
+
+    @FXML
+    private void switchToSecondary() throws IOException {
+        App.setRoot("secondary");
+    }
+}
